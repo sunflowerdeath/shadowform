0044629d59842ad4080a06fac6b6bc6181b24587
Molecular Man <sunflowerdeath@gmail.com>
2018-09-03 00:37:54 +0300

    changes in package.json

--- packages/docs/babel.config.js
+++ packages/docs/babel.config.js
@@ -1,9 +1,1 @@
-const baseConfig = require('gnoll/config/babel')
-
-module.exports = {
-	...baseConfig,
-	plugins: [
-		['@babel/plugin-proposal-decorators', { legacy: true }],
-		['@babel/plugin-proposal-class-properties', { loose: true }]
-	]
-}
+module.exports = require('gnoll/config/babelStage0')
