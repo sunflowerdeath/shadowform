21ee3861945fc6ef6941b0db6148b4bc045392b5
Petr Tripolsky <tripolskypetr@gmail.com>
2022-04-30 17:35:59 +0300

    typedef

---
+++ packages/shadowform/types.d.ts
@@ -0,0 +1,74 @@
+declare module "shadowform" {
+
+    interface IField {
+        normalize?: (value: any) => any;
+        requiredError?: string;
+        validations?: Record<string, {
+            error: String;
+            validate: (value: any, values: any) => boolean;
+        }>;
+        isRequired?: boolean;
+        isEmpty?: boolean;
+        initialValue?: any;
+    }
+
+    interface IFormStoreArgs {
+        fields?: Record<string, IField>;
+        initialValues?: Record<string, any>;
+    }
+
+    interface IFieldStoreArgs extends IField {
+        asyncValidations?: any;
+        asyncValidationDelay?: any;
+        validationErrors?: any;
+        form: FormStore;
+    }
+
+    export class FormStore {
+        fields: Record<string, FieldStore>;
+        initialValues: Record<string, any>;
+        constructor(params: IFormStoreArgs);
+        get enabledFields(): any;
+        get values(): any;
+        get normalizedValues(): any
+        get isValid(): any;
+        get isValidating(): any;
+        reset()
+        setErrors(errors: Record<string, any>)
+    }
+
+    export class FieldStore {
+        get isDisabled(): boolean;
+        get value(): any;
+        get isEmpty(): boolean;
+        get normalizedValue(): any;
+        get isValidating(): boolean;
+        get isValid(): boolean;
+        get error(): any;
+        constructor(params: IFieldStoreArgs);
+        on(...args: any[]): () => void;
+        getError(type: any, validation: any): any;
+        change(value: any);
+        reset();
+        setValue(value: any);
+        cancelValidation();
+        validate();
+        asyncValidate(): Promise<void>;
+        setError(error: any);
+        removeError();
+        setValidating(validating: boolean);
+        disable();
+        enable();
+    }
+
+    export function useShowError(args: {
+        field: FieldStore;
+        showRequiredError: string;
+        showValidationErrors: string;
+    }): {
+        showError: boolean;
+        onFocus: (e: any) => void;
+        onBlur: (e: any) => void;
+    }
+
+}
